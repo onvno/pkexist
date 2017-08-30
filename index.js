3ea9ef179e45f8bab4b66263b3f2441277c0e969
onvno <leewei2020@126.com>
2017-08-30 11:27:57 +0800

    update, 文档暂未升级

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const exists = require('npm-exists');
+const request = require('request');
 const chalk = require('chalk');
 
-function search(module){
-  exists(module)
-	.then(moduleExists => {
-		if (moduleExists) {
-			console.log(chalk.dim(`${module}: Sorry, this package has existed`));
-		} else {
-			console.log(chalk.green(`${module}: Congratulations, this package has't existed`));
-		}
-  });
-}
+function search(moduleName,existlist=[],nonelist=[] ) {
+    return new Promise(function(resolve, reject) {
+        request('https://www.npmjs.org/package/' + moduleName, function(error, response, body) {
+            if (error) {
+                return reject(error);
+            } 
+            if (response.statusCode === 200) {
+								existlist.push(moduleName);
+								// 忽略单条信息反馈
+                return resolve(`{${moduleName}: '已存在'}`);
+            }
+						nonelist.push(moduleName);
+						// 忽略单条信息反馈
+            resolve(`{${moduleName}: '暂时不存在'}`);
+        });
+    });
+};
 
 module.exports = search;
